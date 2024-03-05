4b36938ac39a00c8d04e7934da9ec5318b19636c
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-03-05 11:06:15 +0900

    Add files via upload

--- app/admin/server-management/aws/page.js
+++ app/admin/server-management/aws/page.js
@@ -12,19 +12,35 @@ export default function Page() {
           <p className="block font-bold mb-1">알려주실 정보</p>
           <p>aws 아이디 / 암호</p>
         </div>
-    
+
         <div className="mb-3">
-          <p className="block font-bold mb-1">aws 서버 호스팅</p>
-          aws 1년 무료 서버 호스팅 에 가입 및 호스팅 
+          <p className="block font-bold mb-1">aws 가입</p>
+          <p>aws 1년 무료 서버 호스팅 에 가입</p>
           <a href="https://aws.amazon.com" target="_blank">https://aws.amazon.com</a>
           <img src="/admin/server-management/aws/aws-1.png" />
+        </div>
+            
+        <div className="mb-3">
+          <p className="block font-bold mb-1">aws 서버 호스팅</p>
+          <p>서버 호스팅</p>
           <img src="/admin/server-management/aws/aws-2.png" />
-          봇이 미응답시 시스템을 재부팅 (혹은 중지후 다시 시작) 합니다.
+          <p>봇이 미응답시 시스템을 재부팅 (혹은 중지후 다시 시작) 합니다.</p>
           <img src="/admin/server-management/aws/aws-3.png" />
-          장시간 미사용시 중지 합니다. 
-          완전 삭제시는 종료 합니다.
+          <p>장시간 미사용시 중지 합니다.</p> 
+          <p>완전 삭제시는 종료 합니다.</p>
         </div>
 
+        <div className="mb-3">
+          <p className="block font-bold mb-1">aws 서버 콘솔 접속</p>
+          <p>pem 파일을 다운 받아 콘솔 (윈도우에서는 파워셀)에서 아래 명령어로 접속 하면 됩니다.</p>
+<p>chmod 400 /Users/xxx/Documents/aws/xxx.pem</p>
+<p>ssh -i "/Users/xxx/Documents/aws/xxx.pem" ubuntu@111.222.333.444</p>
+          <br/>
+          <img src="/admin/server-management/aws/aws-4.png" />
+          <br/>
+          <img src="/admin/server-management/aws/aws-5.png" />
+        </div>
+            
       </div>
   );
 }
--- app/admin/server-management/cafe24/page.js
+++ app/admin/server-management/cafe24/page.js
@@ -14,18 +14,34 @@ export default function Page() {
         </div>
     
         <div className="mb-3">
-          <label className="block font-bold mb-1">카페 24 서버 호스팅</label>
-          카페 24 월 5000원 서버 호스팅에 가입 및 호스팅
+          <label className="block font-bold mb-1">카페 24 가입</label>
+          <p>카페에 가입</p>
           <a href="https://hosting.cafe24.com/?controller=new_product_page&page=virtual" target="_blank">https://hosting.cafe24.com/?controller=new_product_page&page=virtual</a>
+          <br/>        
           <img src="/admin/server-management/cafe24/cafe24-1.png" />        
-          봇이 미응답시 시스템을 종료후 다시 시작 합니다. 
-          나의서비스 관리 페이지 접속 &gt; 서버 원격 관리 클릭
-          <img src="/admin/server-management/cafe24/cafe24-2.png" />
-          <img src="/admin/server-management/cafe24/cafe24-3.png" />
-          <img src="/admin/server-management/cafe24/cafe24-4.png" />
-          장시간 미사용시 종료 합니다. 
         </div>
 
+        <div className="mb-3">
+          <label className="block font-bold mb-1">카페 24 서버 호스팅</label>
+          <p>카페 24 월 5000원 서버 호스팅에 호스팅</p>     
+          <p>봇이 미응답시 시스템을 종료후 다시 시작 합니다.</p> 
+          <p>나의서비스 관리 페이지 접속 &gt; 서버 원격 관리 클릭</p>
+          <img src="/admin/server-management/cafe24/cafe24-2.png" />
+          <br/>
+          <img src="/admin/server-management/cafe24/cafe24-3.png" />
+          <br/>
+          <img src="/admin/server-management/cafe24/cafe24-4.png" />
+          <p>장시간 미사용시 종료 합니다.</p> 
+        </div>
+
+        <div className="mb-3">
+          <p className="block font-bold mb-1">카페 24 서버 콘솔 접속</p>
+          <p>pem 파일을 다운 받아 콘솔 (윈도우에서는 파워셀)에서 아래 명령어로 접속 하면 됩니다.</p>
+          <p>ssh -i "/Users/xxx/Documents/cafe24/xxx.pem" ubuntu@111.222.333.444</p>
+          <br/>
+          <img src="/admin/server-management/cafe24/cafe24-5.png" />
+        </div>
+            
       </div>
   );
 }
