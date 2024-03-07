140d8335f14ef4362e2157151e8d707d06aa6524
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-03-07 16:03:47 +0900

    Update Footer.js

--- app/Footers/blank/Footer.js
+++ app/Footers/blank/Footer.js
@@ -2,6 +2,18 @@
 import React, { useState, useEffect } from 'react';
 
 const Footer = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    setLoading(false);
+  }, []);
+
+  if (loading)
+    return <>loading</>;
+
+  if (window.location.pathname === '/chat/web-chat') 
+    return null;
 
   return (
     <>
