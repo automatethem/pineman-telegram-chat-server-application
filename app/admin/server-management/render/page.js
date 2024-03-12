54baec075044c522e659f88afa6886539de0e9be
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-03-13 02:49:14 +0900

    Add files via upload

---
+++ app/admin/server-management/render/page.js
@@ -0,0 +1,19 @@
+"use client";
+import { useEffect, useState } from 'react';
+
+export default function Page() {
+
+  return (
+
+      <div>
+        <p className="mb-3 text-lg font-bold">서버 관리 &gt; render 모델 호스팅 관리 방법</p>
+
+        <div className="mb-3">
+          <label className="block font-bold mb-1">render 모델 호스팅</label>
+          <br/>
+          <a href="https://render.com/" target="_blank">https://render.com/</a>
+        </div>
+
+      </div>
+  );
+}
