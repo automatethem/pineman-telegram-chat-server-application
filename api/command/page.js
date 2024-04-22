265ec402147429e8cc17a93ee2e4f04af5409789
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-04-22 17:46:00 +0900

    Add files via upload

---
+++ api/command/page.js
@@ -0,0 +1,8 @@
+const Page = () => {
+  return <>
+    <div>
+      <p className="mb-4 text-lg font-bold">Api 리스트 관리</p>
+    </div>
+  </>;
+};
+export default Page;
---
+++ api/page.js
@@ -0,0 +1,8 @@
+const Page = () => {
+  return <>
+    <div>
+      <p className="mb-4 text-lg font-bold">Api 관리</p>
+    </div>
+  </>;
+};
+export default Page;
