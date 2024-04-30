4c5a8d3ebfa628c229ddb9e64251cce812b34049
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-04-30 11:29:29 +0900

    Update AdminMenu.js

--- app/admin/AdminMenu.js
+++ app/admin/AdminMenu.js
@@ -1,3 +1,42 @@
+"use client";
+import React, { useState, useEffect } from 'react';
+import Script from 'next/script'
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+ 
+const Page = () => {
+  const [id, setId] = useState(null);
+  const [menu, setMenu] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(async () => {
+    setLoading(true);
+
+    const { data, error } = await supabase
+      .from('AdminSetting')
+      .select('*')
+      .single();
+    if (!error && data) {
+      setId(data.id);
+      setMenu(data.menu);
+    }
+
+    setLoading(false);
+  }, []);
+
+  if (loading)
+    return <>loading</>;
+
+  return (
+    <>
+      <div dangerouslySetInnerHTML={{ __html: menu }} />
+    </>
+  );
+};
+export default Page;
+{/*
 import Page from "./AdminMenus/chat/telegram-chat-without-ai/AdminMenu.js";
 
 export default Page;
+*/}
