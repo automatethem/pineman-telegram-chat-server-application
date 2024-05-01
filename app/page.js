04ef408db42952b101f71b580dad7df92d4a9024
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-05-01 17:33:47 +0900

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -1,13 +1,18 @@
 "use client"
 import { createClient } from '@supabase/supabase-js'
 import React, { useState, useEffect } from 'react';
-import { useRouter } from "next/navigation";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
+import rehypeExternalLinks from 'rehype-external-links';
+//import { useRouter } from "next/navigation";
 //import AiWebChat from "./chat/ai-web-chat/page.js";
 import AiWebChat from "./ai-web-chat-bridge.js";
 
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
 const Page = () => {
+  const [message, setMessage] = useState('');
   const [useForwardToAiWebChatFromHome, setUseForwardToAiWebChatFromHome] = useState(false);
   const [loading, setLoading] = useState(true);
   //const router = useRouter();
@@ -15,6 +20,14 @@ const Page = () => {
   useEffect(async () => {
     setLoading(true);
 
+    const { data, error } = await supabase
+      .from('WebSiteSetting')
+      .select('*')
+      .single();
+    if (!error && data) {
+      setMessage(data.message);
+    }
+    
     const { data: aiWebChatSetting, error: aiWebChatSettingError } = await supabase
       .from('AiWebChatSetting')
       .select('*')
@@ -41,12 +54,24 @@ const Page = () => {
   }
 
   return (
-  <>
-  <p>xx  서비스</p>
-  <p>추천 서비스</p> 
-  <p>신규 서비스</p>
-  <p>인기 서비스</p>
-  </>
+      <div>
+          {/*<h1 className="mb-3 text-lg font-bold">{title}</h1>*/}
+          <div className="mb-3">
+              {/* Using ReactMarkdown to render the message as markdown */}
+              <ReactMarkdown 
+                remarkPlugins={[remarkGfm]}
+                rehypePlugins={[rehypeRaw, [rehypeExternalLinks, { target: '_blank' }]]}
+                components={{
+                  // Custom component for 'a' tag to add className
+                  a: ({ node, ...props }) => (
+                    <a {...props} className="underline text-blue-600 hover:text-blue-800" />
+                  ),
+                }}
+              >
+                  {message}
+              </ReactMarkdown>
+          </div>
+      </div>
   );
 };
 export default Page;
